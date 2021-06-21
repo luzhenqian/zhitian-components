058f5f013301f60a93e7ee4f799c679015c97b23
luzhenqian <15753140326@163.com>
2021-06-21 23:49:28 +0800

    fix: dev vue script

--- examples/ztc-vue/index.js
+++ examples/ztc-vue/index.js
@@ -1,10 +1,6 @@
-// import Counter from "@/examples/vue/counter/index.vue";
-// import Counter from "@/examples/counter/component/index.jsx";
-// import BarChartVue from "@/examples/vue/bar-chart/index.vue";
 import BarChartVue from "@/examples/ztc-vue/bar-chart/index.vue";
 import { createApp } from "vue";
 
-// const app = createApp(Counter);
 const app = createApp(BarChartVue);
 
 const container = document.createElement("div");
